9c04f606cc0f55f40fd3cbb8fb248d7c2170a1cb
seongu99 <seongu99@gmail.com>
2016-08-06 18:43:05 -0400

    check lists of employee
    -

--- managerMain.php
+++ managerMain.php
@@ -1,3 +1,24 @@
 <?php
-	echo 'manager main page';
+
+	require_once('init.php');
+
+	$sql = "SELECT FirstName,LastName FROM person WHERE type='employee'";
+	$execute = mysql_query($sql) or die(mysql_error()); 
+	
+	$total_record = mysql_num_rows($execute);
+	for ($i = 0; $i < $total_record; $i++) {
+
+ 		mysql_data_seek($execute, $i);
+        $row = mysql_fetch_array($execute);
+
+        echo $row["FirstName"];
+        echo " ";
+		echo $row["LastName"];
+		echo "<br />";
+
+
+	}
+	//echo $listofemployee["FirstName"],["LastName"];
+
+	//echo $listofemployee[1];
 ?>
